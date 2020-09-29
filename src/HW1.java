82eb8c4db13e5259417b363941249355e417bc4c
user <user@EC07>
2020-09-29 09:35:30 +0800

    D0843704

--- src/HW1.java
+++ src/HW1.java
@@ -3,7 +3,9 @@ public class HW1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("I'm ¦óÞ·ªY");
+		System.out.println("My nid is D0843704");
+		System.out.println("This is lab 1 hw");
 	}
 
 }

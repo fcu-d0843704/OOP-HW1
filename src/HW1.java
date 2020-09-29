a65314275ed3f1469dd6abaa26867cad0eb8d602
user <user@EC07>
2020-09-29 09:09:40 +0800

    3

--- src/HW1.java
+++ src/HW1.java
@@ -3,7 +3,7 @@ public class HW1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Kitty");
+		System.out.println("Hello World");
 	}
 
 }
